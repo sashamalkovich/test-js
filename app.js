88c6f183316ad03004739fd065f252290515d12e
sashamalkovich <56202519+sashamalkovich@users.noreply.github.com>
2020-04-13 13:02:47 +0300

    new site commit 0.1.2 favicon not worcing; + meta tags + description on ABOUT

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var musicRouter = require('./routes/music');
@@ -24,6 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(favicon ('favicon.ico'));
 
 app.use('/', indexRouter);
 app.use('/music', musicRouter);
